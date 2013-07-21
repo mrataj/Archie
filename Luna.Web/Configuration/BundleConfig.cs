dfd395e62a957896d95d3631af22d443b2f35321
Miha <rataj.miha@gmail.com>
2013-07-21 10:01:45 +0200

    initial

---
+++ Luna.Web/Configuration/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿namespace Luna.Web.Configuration
+{
+  using System.Web.Optimization;
+
+  /// <summary>
+  /// Takes care for bundle configuration.
+  /// </summary>
+  public static class BundleConfig
+  {
+    /// <summary>
+    /// Add bundles to given bundle collection.
+    /// </summary>
+    /// <param name="bundles">Given bundle collection.</param>
+    public static void RegisterBundles(BundleCollection bundles)
+    {
+      bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                  "~/Scripts/jquery-{version}.js"));
+      
+      bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                  "~/Scripts/jquery.unobtrusive*",
+                  "~/Scripts/jquery.validate*"));
+
+      // Use the development version of Modernizr to develop with and learn from. Then, when you're
+      // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+      bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                  "~/Scripts/modernizr-*"));
+
+      bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+    }
+  }
+}
---
+++ Luna.Web/Configuration/FilterConfig.cs
@@ -0,0 +1,19 @@
+﻿namespace Luna.Web.Configuration
+{
+  using System.Web.Mvc;
+
+  /// <summary>
+  /// Takes care for registering all filters.
+  /// </summary>
+  public class FilterConfig
+  {
+    /// <summary>
+    /// Adds filters to given filter collection.
+    /// </summary>
+    /// <param name="filters">Given filter collection.</param>
+    public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+    {
+      filters.Add(new HandleErrorAttribute());
+    }
+  }
+}
