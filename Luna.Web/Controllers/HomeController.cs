8636c0c693fff50dc05a13819f3b180d4e856275
Miha <rataj.miha@gmail.com>
2013-07-30 23:00:52 +0200

    added scriptsharp

---
+++ Luna.Web/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿namespace Luna.Web.Controllers
+{
+  using System.Web.Mvc;
+
+  /// <summary>
+  /// Simple home controller.
+  /// </summary>
+  public class HomeController : Controller
+  {
+    /// <summary>
+    /// Gets default view.
+    /// </summary>
+    /// <returns>View result with home view.</returns>
+    public ActionResult Index()
+    {
+      return this.View("~/Views/Home/Home.cshtml");
+    }
+  }
+}
