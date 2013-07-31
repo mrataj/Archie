93ecb075d07720dda0148f9c4379623f508963e6
Miha Rataj <rataj.miha@gmail.com>
2013-07-31 08:30:17 +0200

    added home model

---
+++ Luna.Web/Models/HomeModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Luna.Web.Models
+{
+  /// <summary>
+  /// Model for home view.
+  /// </summary>
+  public class HomeModel
+  {
+    /// <summary>
+    /// Gets or sets message.
+    /// </summary>
+    public string Message { get; set; }
+  }
+}
