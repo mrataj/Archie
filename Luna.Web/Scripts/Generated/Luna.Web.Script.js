37f751b98d33e511239e211c511f14c2542368b8
Miha <rataj.miha@gmail.com>
2013-08-18 10:47:04 +0200

    buildfix

--- Luna.Web/Scripts/Generated/Luna.Web.Script.js
+++
@@ -1,4 +0,0 @@
-// Luna.Web.Script.js
-(function(){
-Type.registerNamespace('Archie.Web.Script.Pages');Archie.Web.Script.Pages.Page=function(){window.document.title='Hello, Archie!';}
-Archie.Web.Script.Pages.Page.registerClass('Archie.Web.Script.Pages.Page');})();// This script was generated using Script# v0.7.4.0
