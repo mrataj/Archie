30572fb2c35340c99953db4a0d0b21e6822308fd
Miha Rataj <rataj.miha@gmail.com>
2013-09-17 16:53:06 +0200

    prepared interface for route parser

---
+++ Archie.Web.Script/Engine/IRouteParser.cs
@@ -0,0 +1,25 @@
+﻿namespace Archie.Web.Script.Engine
+{
+  using System.Collections.Generic;
+  using Archie.Web.Script.Models;
+
+  /// <summary>
+  /// Route parser interface.
+  /// </summary>
+  internal interface IRouteParser
+  {
+    /// <summary>
+    /// Register route collection.
+    /// </summary>
+    /// <param name="routes">Collection with routes.</param>
+    void RegisterRoutes(List<RouteModel> routes);
+
+    /// <summary>
+    /// Gets url from route name and given parameters.
+    /// </summary>
+    /// <param name="routeName">Route name.</param>
+    /// <param name="parameters">Url parametrs.</param>
+    /// <returns>Generated url.</returns>
+    string RouteUrl(string routeName, Dictionary<string, string> parameters);
+  }
+}
---
+++ Archie.Web.Script/Engine/MvcRouteParser.cs
@@ -0,0 +1,33 @@
+﻿namespace Archie.Web.Script.Engine
+{
+  using System.Collections.Generic;
+
+  using Archie.Web.Script.Models;
+
+  /// <summary>
+  /// ASP.NET MVC route parser engine.
+  /// </summary>
+  internal class MvcRouteParser : IRouteParser
+  {
+    /// <summary>
+    /// Register route collection.
+    /// </summary>
+    /// <param name="routes">Collection with routes.</param>
+    public void RegisterRoutes(List<RouteModel> routes)
+    {
+      // todo - implement me
+    }
+
+    /// <summary>
+    /// Gets url from route name and given parameters.
+    /// </summary>
+    /// <param name="routeName">Route name.</param>
+    /// <param name="parameters">Url parametrs.</param>
+    /// <returns>Generated url.</returns>
+    public string RouteUrl(string routeName, Dictionary<string, string> parameters)
+    {
+      // todo - implement me
+      return null;
+    }
+  }
+}
