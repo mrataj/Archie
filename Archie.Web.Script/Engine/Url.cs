f7a17eabb8ff8871454e0447392dfc5970d55ca2
Miha Rataj <rataj.miha@gmail.com>
2013-09-17 16:53:01 +0200

    prepared interface for route parser

--- Archie.Web.Script/Engine/Url.cs
+++ Archie.Web.Script/Engine/Url.cs
@@ -1,20 +1,28 @@
 ﻿namespace Archie.Web.Script.Engine
 {
   using System.Collections.Generic;
+  using System.Runtime.CompilerServices;
 
   using Archie.Web.Script.Models;
 
   /// <summary>
   /// Main class for dealing with routes.
   /// </summary>
-  internal static class Url
+  [IgnoreNamespace]
+  public static class Url
   {
+    /// <summary>
+    /// Route parser singleton.
+    /// </summary>
+    private static MvcRouteParser parser;
+
     /// <summary>
     /// Register route collection.
     /// </summary>
     /// <param name="routes">Collection with routes.</param>
-    internal static void RegisterRoutes(List<RouteModel> routes)
+    public static void RegisterRoutes(List<RouteModel> routes)
     {
+      GetEngineImplementation().RegisterRoutes(routes);
     }
 
     /// <summary>
@@ -23,9 +31,24 @@
     /// <param name="routeName">Route name.</param>
     /// <param name="parameters">Url parametrs.</param>
     /// <returns>Generated url.</returns>
-    internal static string RouteUrl(string routeName, Dictionary<string, string> parameters)
+    public static string RouteUrl(string routeName, Dictionary<string, string> parameters)
     {
-      return null;
+      return GetEngineImplementation().RouteUrl(routeName, parameters);
+    }
+
+    /// <summary>
+    /// Gets engine implementation.
+    /// </summary>
+    /// <returns></returns>
+    private static IRouteParser GetEngineImplementation()
+    {
+      if (parser == null)
+      {
+        // todo: get from parameter?
+        parser = new MvcRouteParser();
+      }
+
+      return parser;
     }
   }
 }
